5554621461d87b88e16084399e714bf77cef7d59
Tinko <TinkoLiu@users.noreply.github.com>
2019-12-19 12:31:32 +0800

    feat(login): Add wework login flow

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,7 +2,37 @@ import router from '@/router';
 
 export const isDev = false; // process.env.NODE_ENV === 'development';
 
+function callWeWorkLogin(corpid: string) {
+  const params = new URLSearchParams();
+  params.set('appid', corpid);
+  params.set('response_type', 'code');
+  params.set('scope', 'snsapi_base');
+  params.set(
+    'redirect_uri',
+    encodeURI(window.location.origin + window.location.pathname),
+  );
+  params.set('state', 'wework_redirect');
+  const url =
+    'https://open.weixin.qq.com/connect/oauth2/authorize?' +
+    params.toString() +
+    '#wechat_redirect';
+  window.location.href = url;
+}
+
 export function gotoLogin() {
+  if (window.hasOwnProperty('WeixinJSBridge')) {
+    // Now we are in WeWork / WeChat
+    //  TODO: Check if Desktop WeWork
+    //  TODO: If ture, open in default browser instead.
+    const params = new URLSearchParams(window.location.search);
+    if (params.get('scope') === 'wework_redirect') {
+      // TODO: call backend for adding user / auto login, push route to home
+    } else {
+      // TODO: get company by url search param, or give a selection in 4 company
+      callWeWorkLogin('wxe2be6e5c62e7b072');
+    }
+    return;
+  }
   let query = {};
   if (!/\/login/.test(router.currentRoute.fullPath)) {
     query = { backPath: router.currentRoute.fullPath };
