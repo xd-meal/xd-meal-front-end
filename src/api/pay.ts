a039425e5189a3cd2f8775354b9f337e84ad4797
Tinko <TinkoLiu@users.noreply.github.com>
2020-01-01 15:52:11 +0800

    feat: update pay page

--- src/api/pay.ts
+++ src/api/pay.ts
@@ -2,7 +2,7 @@ import { defaultResponse } from '@/api/common';
 import { IHttpResponse } from '@/api/http';
 import axios from 'axios';
 
-const GET_DISH_API = '/api/v1/GetDishCode';
+const GET_DISH_API = '/api/v1/myDish';
 
 export async function payCode(): Promise<IHttpResponse> {
   const response = await axios.get(GET_DISH_API);
